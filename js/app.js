e4319bfddfafb2a4ae4aa705f305fd11dcb67c79
sather33 <court.dream3@gmail.com>
2020-09-16 20:17:54 +0800

    init state

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
 window.onload = function() {
   console.log("window loaded")
+  initState();
 };
+
+function loadDataToStorage() {
+  console.log('load data')
+  sessionStorage.setItem('restaurants', JSON.stringify(data))
+}
+
+
+function getState() {
+  const state = sessionStorage.getItem('restaurants');
+
+  return JSON.parse(state)
+}
+
+function initState () {
+  loadDataToStorage();
+  const data = getState();
+
+  console.log('initState', data)
+}
